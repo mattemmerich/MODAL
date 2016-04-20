e4845da76dd4618b0c07b5d42c97d2fa3cd5e478
matt emmerich <matt.emmerich@gmail.com>
2016-04-20 10:48:24 -0400

    re-submit

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-    // Get the modal
-    var modal = document.getElementById('myModal');
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+$(document).ready(function(){
 
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
+	$('#open').on('click', function(){
+		$('#shade').show();
+	});
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-});
+		$('#shade').on('click', function(){
+			$('#shade').hide();
+		
+		})
+	
+})
